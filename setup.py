8aa5fc8cf2b05696207836b6136c8282a5acd401
Tony Hirst <tony.hirst@gmail.com>
2018-05-16 12:12:35 +0100

    Add missing dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='ipython_magic_folium',
-	install_requires=['folium','fiona'],
+	install_requires=['folium','fiona', 'geocoder'],
       packages=['folium_magic']
 )
